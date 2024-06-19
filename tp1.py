8b2b448dd3fe010c0583dbe5e5d627363f4a3181
Juanjuancito1 <milei9041@gmail.com>
2024-06-19 11:25:47 -0300

    Add files via upload

---
+++ tp1.py
@@ -0,0 +1,8 @@
+entero = 5555
+flotante = 3.14983
+cadena = "soyrecrack"
+TrueOrFalse = True
+print("Booleano;", TrueOrFalse)
+print("Cadena:", cadena)
+print("Flotante:", flotante)
+print("Entero:", entero)
